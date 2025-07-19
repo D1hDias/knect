23e4df24ab66a643bf3c66f086b999b99af8481f
D1hDias <diego@diasconsultor.com>
2025-07-19 17:10:32 -0300

    sdsds

--- server/auth.ts
+++ server/auth.ts
@@ -14,7 +14,10 @@ export function setupAuth(app: Express) {
         secure: process.env.NODE_ENV === "production",
         httpOnly: true,
         maxAge: 24 * 60 * 60 * 1000, // 24 horas
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        domain: process.env.NODE_ENV === "production" ? ".knect.com.br" : undefined,
       },
+      proxy: process.env.NODE_ENV === "production",
     })
   );
 }
